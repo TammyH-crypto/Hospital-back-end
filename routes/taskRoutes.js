36a4e0611b98438133826556be1167ec9decf64a
Tammy Higgins <tmhiggins1986@gmail.com>
2025-07-15 19:09:43 -0500

    udated tasks

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import {
-  gettask,
-  posttask,
-  puttask,
+  getTask,
+  postTask,
+  putTask,
 } from "../controllers/tasksController.js";
 
 export const tasks = express.Router();
 
-tasks.get(`/task `, gettask);
-tasks.post(`/task`, posttask);
+tasks.get(`/task`, getTask);
+tasks.post(`/task`, postTask);
+tasks.put(`/task/:task_id`, putTask);
 
 
