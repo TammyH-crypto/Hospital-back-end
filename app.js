f838dfe78b0256d8223e588bcb9e3fc1452abaaa
Tammy Higgins <tmhiggins1986@gmail.com>
2025-07-16 20:40:47 -0500

    update cors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import { tasks } from "./routes/taskRoutes.js"
 import { employees } from "./controllers/tasksController.js";
 const app = express();
-app.use(cors());
+app.use(cors({origin: "*" }));
 app.use(express.json());
 app.use("/employees", employeeRoutes);
 app.use("/tasks", taskRoutes);
