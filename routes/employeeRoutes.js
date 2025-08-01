17172eca5ba70cd4ef5fe3ef45f9f442192a4b9f
Tammy Higgins <tmhiggins1986@gmail.com>
2025-07-31 20:52:07 -0500

    added validation token

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -6,11 +6,12 @@ import {
   deleteEmployees,
   getEmployeesId,
 } from "../controllers/EmployeesController.js";
+import { validateToken } from "../middleware/authValidation.js";
 
 export const employees = express.Router();
 
-employees.get(`/employees/:employee_id`, getEmployeesId);
-employees.get(`/employees`, getEmployees);
-employees.post(`/employees`, postEmployees);
-employees.put(`/employees/:employee_id`, putEmployees);
-employees.delete(`/employees/:employee_id`, deleteEmployees);
+employees.get(`/employees/:employee_id`, validateToken, getEmployeesId);
+employees.get(`/employees`, validateToken, getEmployees);
+employees.post(`/employees`, validateToken, postEmployees);
+employees.put(`/employees/:employee_id`, validateToken, putEmployees);
+employees.delete(`/employees/:employee_id`, validateToken, deleteEmployees);
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -4,13 +4,14 @@ import {
   postTasks,
   putTasks,
   deleteTasks,
-  getTasksByEmployeeId
+  getTasksByEmployeeId,
 } from "../controllers/tasksController.js";
+import { validateToken } from "../middleware/authValidation.js";
 
 export const tasks = express.Router();
 
-tasks.get(`/task`, getTasks);
-tasks.get(`/task/:employee_id`, getTasksByEmployeeId);
-tasks.post(`/task`, postTasks);
-tasks.put(`/task/:task_id`, putTasks);
-tasks.delete(`/task/:task_id`, deleteTasks);
+tasks.get(`/task`, validateToken, getTasks);
+tasks.get(`/task/:employee_id`, validateToken, getTasksByEmployeeId);
+tasks.post(`/task`, validateToken, postTasks);
+tasks.put(`/task/:task_id`, validateToken, putTasks);
+tasks.delete(`/task/:task_id`, validateToken, deleteTasks);
